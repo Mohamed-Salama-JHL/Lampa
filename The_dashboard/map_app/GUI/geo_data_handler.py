f6d983c7369c735a8b8a87d5d83c60305c9f9b64
Mohamed-Salama-JHL <131959172+Mohamed-Salama-JHL@users.noreply.github.com>
2023-11-03 10:55:43 +0000

    change dataset layout to df, change zoom level, add globe geojson

--- The_dashboard/map_app/GUI/geo_data_handler.py
+++ The_dashboard/map_app/GUI/geo_data_handler.py
@@ -55,9 +55,12 @@ class geojson_handler:
 
         for feature in cur_geo_data['features']:
             geometry = feature['geometry']
+            coords = []
             if geometry['type'] == 'Polygon':
-                coords = geometry['coordinates'][0]  # For Polygon, the coordinates are nested in another list
-
+                coords = geometry['coordinates'][0]  
+            elif geometry['type'] == 'MultiPolygon':
+                for polygon_coords in geometry['coordinates']:
+                    coords.extend(polygon_coords[0])
             for lon, lat in coords:
                 total_lon += lon
                 total_lat += lat
@@ -108,15 +111,41 @@ class geojson_handler:
 
         return largest_distance
 
+    def _get_largest_distance_2(self,geo_data):
+        largest_distance = 0
+        min_latitude = 100000000
+        min_longitude = 100000000
 
+        max_latitude = -100000000
+        max_longitude = -100000000
+        for feature in geo_data['features']:
+            geometry = feature['geometry']
+            if geometry['type'] == 'Polygon':
+                coords = geometry['coordinates'][0]  
+            elif geometry['type'] == 'MultiPolygon':
+                coords = [c for part in geometry['coordinates'] for c in part[0]]  
+            else:
+                continue
+            try:
+                for i, (lon1, lat1) in enumerate(coords):
+                    min_latitude = min(min_latitude,lat1)
+                    min_longitude = min(min_longitude,lon1)
+
+                    max_latitude = max(max_latitude,lat1)
+                    max_longitude = max(max_longitude,lon1)
+            except:
+                print(feature)
+        largest_distance = haversine_distance(min_longitude, min_latitude, max_longitude, max_latitude)
+        return largest_distance
     def get_zoom_start(self,geo_data=None):
         cur_geo_data = self.return_geojson
         if geo_data !=None:
             cur_geo_data = geo_data
         if cur_geo_data==None:
             return 0
-        actual_distance = self._get_largest_distance(cur_geo_data)
-        
+        #return self.get_zoom_points(cur_geo_data)
+        actual_distance = self._get_largest_distance_2(cur_geo_data)
+        '''
         point1 = (0.3, 17)
         point2 = (228, 5.2)
         point3 = (161, 7)
@@ -128,8 +157,34 @@ class geojson_handler:
 
         output = a * actual_distance ** 3 + b * actual_distance ** 2 + c * actual_distance + d
         output = max(1, min(17, output))
-    
-        return int(output) 
+        '''
+        zoom_levels = {
+            0: 156412,
+            1: 78206,
+            2: 39103,
+            3: 19551,
+            4: 9776,
+            5: 4888,
+            6: 2444,
+            7: 1222,
+            8: 611,
+            9: 305,
+            10: 152,
+            11: 76,
+            12: 38,
+            13: 19,
+            14: 10,
+            15: 5,
+            16: 2,
+            17: 1,
+            18: 0.5
+        }
+        
+        # Find the closest predefined zoom level for the given distance
+        closest_zoom = min(zoom_levels.keys(), key=lambda x: abs(zoom_levels[x] - (actual_distance )))
+
+        print(actual_distance,closest_zoom)
+        return int(closest_zoom) -1
     
 
     def get_all_fields(self):
--- The_dashboard/map_app/main.py
+++ The_dashboard/map_app/main.py
@@ -3,11 +3,25 @@ from bokeh.embed import server_document
 from GUI.map_page import createApp
 from bokeh.settings import settings
 import logging
-
+import sys
 
 logging.basicConfig( level=logging.ERROR,force=True)
+'''
+# Create a custom logging handler that redirects stdout to the logger
+class StreamToLogger:
+    def __init__(self, logger, log_level=logging.DEBUG):
+        self.logger = logger
+        self.log_level = log_level
+        self.linebuf = ''
 
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.log_level, line.rstrip())
 
+# Redirect stdout and stderr to the custom logging handler
+sys.stdout = StreamToLogger(logging.getLogger('stdout'), logging.INFO)
+sys.stderr = StreamToLogger(logging.getLogger('stderr'), logging.ERROR)
+'''
 settings.resources = 'cdn'
 
 pn.serve(createApp,
