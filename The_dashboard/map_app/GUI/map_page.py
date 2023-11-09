f2653c2af016cec17dc31a4a2ec27a1e7beffa18
Mohamed-Salama-JHL <131959172+Mohamed-Salama-JHL@users.noreply.github.com>
2023-11-09 11:26:33 +0000

    remove unused imports

--- The_dashboard/map_app/GUI/map_page.py
+++ The_dashboard/map_app/GUI/map_page.py
@@ -1,10 +1,7 @@
 import numpy as np
 import param
-from bokeh.models import ColumnDataSource
-from bokeh.plotting import figure
 import panel as pn
 import numpy as np
-import hvplot.pandas  # noqa
 import hvplot.dask
 import pandas as pd
 import folium
@@ -13,17 +10,12 @@ import hvplot.pandas
 import pandas as pd
 import numpy as np
 from io import StringIO
-from bokeh.plotting import figure
-from bokeh.models import CategoricalColorMapper,CheckboxGroup, CustomJS
-#import matplotlib.pyplot as plt
 from urllib.request import urlopen
 import json
 import plotly.express as px
 import plotly.graph_objs as go
-import holoviews as hv
 import threading
-from panel.theme import Bootstrap, Material, Native,Fast
-from panel.theme.bootstrap import BootstrapDarkTheme
+from panel.theme import Native
 from .data_handler import *
 from .geo_data_handler import * 
 from .styles import *
